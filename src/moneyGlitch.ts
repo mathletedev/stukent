473a6f6013b0e3817933a8c0747a3a266d5b07a8
Neal Wang <nealwang.sh@protonmail.com>
2025-03-03 09:25:19 -0800

    feat: loans

--- src/moneyGlitch.ts
+++ src/moneyGlitch.ts
@@ -22,6 +22,10 @@ export const moneyGlitch = (amount: string) => {
 
     const pong = ['{"msg":"pong"}'];
 
+    const loan = [
+        '{"msg":"method","id":"47","method":"applyForPersonalLoan","params":[{"userId":"DMHRTdwjs99vYF3GZ","courseKey":"CC-5B6855","propertyId":0,"title":"Personal Loan","loanType":3,"interestRate":6,"loanTenure":"3","loanAmount":10000,"downpayment":0,"loanFees":500,"paidOff":false,"propertyValue":0,"totalFinanced":10500,"monthlyPayment":319.43,"totalPayment":11499.48,"offerPrice":0,"closingCost":0,"creditScore":785,"PI":319.43,"TI":0,"loanStatus":"Pending"}]}',
+    ];
+
     socket.onopen = () => {
         socket.send(JSON.stringify(connect));
         socket.send(JSON.stringify(login));
